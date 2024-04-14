867122b160087556e072a4f170453cf8e4541607
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2024-04-14 22:44:00 +0900

    opencv and gst camera are working

--- src/inference.py
+++ src/inference.py
@@ -5,62 +5,6 @@ import beachbot
 
 import time
 
-# def wrap_detection(input_image, output_data):
-#     class_ids = []
-#     confidences = []
-#     boxes = []
-
-#     rows = output_data.shape[0]
-#     image_width, image_height, _ = input_image.shape
-
-#     x_factor = 1
-#     y_factor = 1
-
-#     for r in range(rows):
-#         row = output_data[r]
-#         confidence = row[4]
-
-#         if confidence >= 0.2:
-
-#             classes_scores = row[5:]
-#             class_id = np.argmax(classes_scores)
-
-#             if classes_scores[class_id] > 0.25:
-#                 confidences.append(confidence)
-
-#                 class_ids.append(class_id)
-
-#                 x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item()
-#                 left = int((x - 0.5 * w) * x_factor)
-#                 top = int((y - 0.5 * h) * y_factor)
-#                 width = int(w * x_factor)
-#                 height = int(h * y_factor)
-#                 box = np.array([left, top, width, height])
-#                 boxes.append(box)
-
-#     indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
-
-#     result_class_ids = []
-#     result_confidences = []
-#     result_boxes = []
-
-#     for i in indexes:
-#         result_confidences.append(confidences[i])
-#         result_class_ids.append(class_ids[i])
-#         result_boxes.append(boxes[i])
-
-#     return result_class_ids, result_confidences, result_boxes
-
-
-# def apply_model(location, inputs):
-#     session = onnxruntime.InferenceSession(location)
-#     if session is None:
-#         raise ValueError("Failed to load the model")
-#     inputs = np.swapaxes(np.swapaxes(inputs, 0, -1), -2, -1)[None, :, :, :] / 255
-#     inputs = inputs.astype(np.float16)
-#     prediction = session.run(None, {"images": inputs})
-#     return prediction
-
 
 def set_resolution(intended_x, intended_y, image):
     h = image.shape[0]
@@ -100,8 +44,8 @@ def draw_box(class_ids, confidences, boxes, image, config):
 
 
 def main():
-    # cam = beachbot.sensors.JetsonGstCameraNative()
-    cam = beachbot.sensors.JetsonCsiCameraOpenCV()
+    cam = beachbot.sensors.JetsonGstCameraNative()
+    #cam = beachbot.sensors.JetsonCsiCameraOpenCV()
     viewer = beachbot.utils.ImageViewerMatplotlib
     detector = beachbot.ai.Yolo5Onnx(
         "../Models/beachbot_yolov5s_beach-cleaning-object-detection__v2i__yolov5pytorch_1280/best.onnx"
