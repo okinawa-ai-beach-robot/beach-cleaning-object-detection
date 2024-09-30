6efa4f51f3c559677df47170e38df1d8403e48b6
jeffreyfq <jeffrey.queisser@oist.jp>
2024-09-30 10:30:41 +0900

    modified server training script to use yolov5 pip package and added commandline arguments

--- scripts/Train_YOLO_server.py
+++ scripts/Train_YOLO_server.py
@@ -12,11 +12,21 @@ import shutil
 from subprocess import call
 import argparse
 import sys
+import yaml
+import shutil
+import subprocess
 
 import yolov5
-from yolov5 import train, val, detect, export
+from yolov5 import train, val, detect, export, utils
 import yolov5.export
 import yolov5.train
+
+
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+def get_git_revision_short_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
 
 # Define path to yolo github repository that is equal to the pip base folder:
@@ -25,10 +35,16 @@ yolo_path = os.path.dirname(yolov5.__file__)
 
 def parse_args():
     parser=argparse.ArgumentParser(description="a script to do stuff")
-    parser.add_argument("dataset_location", nargs='?', help="path to dataset root directory", type=str, default="../Datasets/beach-cleaning-object-detection.v8-yolotrain.yolov5pytorch/")
-    parser.add_argument("yolo_modeltype", nargs='?', help="yolo model type, e.g. yolov5s", type=str, default="yolov5s")
-    parser.add_argument("fintune", nargs='?', help="modify only output layer instead of all weights", type=bool, default=False)
-    
+    parser.add_argument("--dataset_location", nargs='?', help="path to dataset root directory", type=str, default="../Datasets/trash_detection.minimal_example.yolov5pytorch/")
+    parser.add_argument("--yolo_modeltype", nargs='?', help="yolo model type, e.g. yolov5s", type=str, default="yolov5s")
+    parser.add_argument("--fintune", nargs='?', help="modify only output layer instead of all weights", type=bool, default=False)
+    parser.add_argument('--img_widths', nargs="*", default=[1280, 640, 320, 160])
+    parser.add_argument("--num_epochs", nargs='?', type=int, default=800)
+    parser.add_argument("--output_folder", nargs='?', help="Base folder for storage of trained models", type=str, default="../Models/")
+    parser.add_argument("--force_retrain", nargs='?', type=bool, default=False)
+    parser.add_argument("--device", nargs='?', help="Device can be cpu, mps or 0...n for cuda devices", type=str, default="cpu")
+    parser.add_argument("--half_precision", nargs='?', help="Export in half precision format (only available on cuda training device, i.e. when --device 0 or similar is given)", type=bool, default=False)
+
     
     
     args=parser.parse_args()
@@ -38,25 +54,28 @@ def parse_args():
 
 
 def modeltrain():
-    inputs=parse_args()
-    print(inputs)
+    args=parse_args()
+    print(args)
 
 
-    # Define dataset:
+    # Define dataset folder:
+    #
     # Unzipped download from roboflow webpage:
-    #dataset_location = "../Datasets/beach-cleaning-object-detection.v1i.yolov5pytorch/"
+    #dataset_location = "../Datasets/beach-cleaning-object-detection.v1i.yolov5pytosrch/"
     #dataset_location = "../Datasets/beach-cleaning-object-detection.v2i.yolov5pytorch/"
     # or; dataset_location = "../Datasets/beach-cleaning-object-detection.v1i.yolov5pytorch/"
-
+    #
     # v2 + data augmentation: approx 3x500 samples, 6 classes:
     #dataset_location = "../Datasets/beach-cleaning-object-detection.v3-augmented_ver.2.yolov5pytorch/"
-
+    #
     # Extended dataset with robot images and further images recorded, state September 9th 2024
-    #dataset_location = "../Datasets/beach-cleaning-object-detection.v8-yolotrain.yolov5pytorch/"
-    dataset_location = "../Datasets/trash_detection.minimal_example.yolov5pytorch/"
+    # e.g. `dataset_location = "../Datasets/beach-cleaning-object-detection.v8-yolotrain.yolov5pytorch/"
+    #       dataset_location = "../Datasets/trash_detection.minimal_example.yolov5pytorch/"
+    dataset_location = os.path.abspath(args.dataset_location)
 
     # Define model variant:
-    yolo_modeltype = "yolov5s"
+    # e.g. yolo_modeltype = "yolov5s"
+    yolo_modeltype = args.yolo_modeltype
 
 
 
@@ -68,19 +87,8 @@ def modeltrain():
 
 
 
-    # Add yolo code folder to python, and import their utils class:
-    sys.path.append(yolo_path) 
-    try:
-        import utils
-    except Exception as ex:
-        print("Error:", ex, "\n\n")
-        print("Check if current path is in folder 'Networks', yolov5 was downloaded from git, and that yolov5 is located in correct (git-root) folder.")
-        print("It may be necessary to click Kernel->Restart and try again...")
-        
-
 
     # Read number of classes based on YAML config file:
-    import yaml
     with open(dataset_location + "/data.yaml", 'r') as stream:
         data_cfg = yaml.safe_load(stream)
         num_classes = str(data_cfg['nc'])
@@ -95,11 +103,8 @@ def modeltrain():
         
 
 
-    # ## Update path information in dataset for yolo training:
-
-    # In[ ]:
-
-
+    # Update path information in dataset for yolo training:
+    #
     # Read current dataset config:
     with open(dataset_location + "/data.yaml", 'r') as stream:
         data_cfg = yaml.safe_load(stream)
@@ -114,7 +119,6 @@ def modeltrain():
         yaml.dump(data_cfg, file)
 
 
-    # In[ ]:
 
 
     # This is the final dataset configuration file:
@@ -125,11 +129,8 @@ def modeltrain():
     file.close()
 
 
-    # In[ ]:
-
 
     # load the model configuration we will use
-
     with open(yolo_path + "/models/" + yolo_modeltype + ".yaml", 'r') as stream:
         model_def = yaml.safe_load(stream)
 
@@ -137,10 +138,7 @@ def modeltrain():
     pprint(model_def)
 
 
-    # ## Write model configuration file for yolo training:
-
-
-
+    # Write model configuration file for yolo training:
     # modify number of classes for training:
     model_def['nc']=num_classes
         
@@ -152,34 +150,32 @@ def modeltrain():
     pprint(model_def)
 
 
-    # ## Start training and collect models
-    # * trained models are stored in folder: beach-cleaning-object-detection/Models/
+    # Start training and collect models
+    eval_img_widths = args.img_widths
+    num_epochs = args.num_epochs
+    device=args.device
 
-
-
-
-    eval_img_widths = [1280, 640, 320, 160]
-    num_epochs = 2
+    out_folder = os.path.abspath(args.output_folder)
 
     for img_width in reversed(eval_img_widths):
+        img_width = int(img_width)
         targetfolder = "beachbot_" + yolo_modeltype + "_" + dataset_id + "_" + str(img_width)
-        targetpath = yolo_path + "runs/train/" + targetfolder + "_results/"
-        resultspath = "../Models/" + targetfolder + "/"
+        targetpath = "runs/train/" + targetfolder + "_results/"
+        resultspath = out_folder + "/" + targetfolder + "/"
         #check if solder exists -> skip computation!
-        if os.path.isdir(targetpath) or os.path.isdir(resultspath):
+        
+        if (os.path.isdir(targetpath) or os.path.isdir(resultspath)) and not args.force_retrain:
             print("Skipping run", img_width, ":", targetfolder, " or", resultspath,  " (resultfolders) exist!")
         else:
+            # if yolo output folder exists, delete it, as yolo will add suffixes -1..-n to the results folder otherwise:
+            if os.path.isdir(targetpath): 
+                shutil.rmtree(targetpath)
             print("Start run", img_width)
-            #params=("--img " + str(img_width) + " --batch 16 --epochs " + str(num_epochs) + " --data " + dataset_location + "/data.yaml --cfg " + yolo_path + "/models/beachbot_" + yolo_modeltype + ".yaml --weights " +  yolo_modeltype + ".pt --name " + targetfolder + "_results  --cache").split()
-            yolov5.train.run(img=img_width, batch=16, epochs=num_epochs, data=dataset_location + "/data.yaml", cfg=yolo_path + "/models/beachbot_" + yolo_modeltype + ".yaml", weights=yolo_modeltype + ".pt", name=targetfolder + "_results", cache=True  )
-            #call(["python", yolo_path+"train.py"] + params)
-            
+            yolov5.train.run(img=img_width, batch=16, epochs=num_epochs, data=dataset_location + "/data.yaml", cfg=yolo_path + "/models/beachbot_" + yolo_modeltype + ".yaml", weights=yolo_modeltype + ".pt", name=targetfolder + "_results", cache=True, device=device  )
+
             # Training done
             # Following files in targetfolder are important:
-            # weights/best.pt 
-            # weights/last.pt
-            # results.csv
-            resultfiles = ["weights/best.pt","weights/last.pt","results.csv"]
+            resultfiles = ["weights/best.pt","results.csv", "opt.yaml"] # optional: "weights/last.pt"
 
             # Create results folder
             try: 
@@ -206,20 +202,20 @@ def modeltrain():
             img_width = img_width,
             img_width_export = img_width_export,
             img_heigt = img_heigt,
-            img_heigt_export = img_heigt_export
+            img_heigt_export = img_heigt_export,
+            train_args = str(args),
+            train_version = get_git_revision_hash()
         )
         with open(resultspath + "export_info.yaml", 'w') as outfile:
             yaml.dump(data, outfile, default_flow_style=False)
 
         # - Export model to onnx
-        # - Export to tensorRT
-        # - Export to tensorflow.js
+        # - TODO Export to tensorRT
+        # - TODO Export to tensorflow.js
         #https://github.com/NVIDIA/TensorRT/blob/main/quickstart/SemanticSegmentation/tutorial-runtime.ipynb
         #if not os.path.isfile(resultspath + "best.onnx") or not os.path.isfile(resultspath + "best.engine") or not os.path.isdir(resultspath + "best_web_module/"):
         print("Export run", img_width)
-        #params_export = ("--weights " + resultspath + "best.pt  --img " + str(img_heigt_export) + " " + str(img_width_export) + " --data " + dataset_location + " --include onnx" + " --opset 12 --simplify --device 0 --half").split() 
-        #call(["python", yolo_path+"export.py"] + params_export)
-        yolov5.export.run(weights=resultspath + "best.pt", img=str(img_heigt_export) + " " + str(img_width_export), data=dataset_location, include="onnx", opset=12, simplify=True, device=False, half=True )
+        yolov5.export.run(weights=resultspath + "best.pt", img=(img_heigt_export, img_width_export), data=dataset_location, include=["onnx",], opset=12, simplify=True, device=device, half=args.half_precision )
             
             
                 
@@ -231,8 +227,11 @@ def modeltrain():
 
 
 if __name__ == '__main__':
-    modeltrain()
-
+    try:
+        modeltrain()
+    except AttributeError as ex:
+        print("Error: ", ex)
+        print("If error is related to FreeType, try downgrading pillow, e.g. pip install Pillow==9.5.0")
 
 
 
