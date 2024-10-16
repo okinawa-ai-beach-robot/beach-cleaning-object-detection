b3a17d80dca0a5ef8aec00ba9bd55222e0faeb54
Christopher Buckley <topherbuckley@gmail.com>
2024-10-16 17:24:50 -0400

    Updating get_model args to allow version and resolution selection

--- src/beachbot_od/huggingface_get_model.py
+++ src/beachbot_od/huggingface_get_model.py
@@ -2,12 +2,15 @@ from huggingface_hub import hf_hub_download
 from pathlib import Path
 
 
-def get_model(model_name="yolo_v5_beachbot_160") -> Path:
+def get_model(version: str, resolution: int) -> Path:
+    model_name = f"yolo_v5_beachbot_{resolution}"
     repo_path = f"okinawa-ai-beach-robot/{model_name}"
-    model_path = hf_hub_download(repo_id=repo_path, filename="model.pt")
+    model_path = hf_hub_download(
+        repo_id=repo_path, filename="model.pt", revision=version
+    )
     model_path = Path(model_path)
     return model_path
 
 
 if __name__ == "__main__":
-    get_model(model_name="yolo_v5_beachbot_160")
+    get_model(version="v1.0", resolution=160)
