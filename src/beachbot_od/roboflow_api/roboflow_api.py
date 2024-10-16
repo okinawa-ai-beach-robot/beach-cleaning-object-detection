473a5b7fe9da4bfaebd8939c19d050ec8fce6aae
Christopher Buckley <topherbuckley@gmail.com>
2024-10-16 18:03:18 -0400

    Merging roboflow api into a single file for better reuse

--- src/beachbot_od/roboflow_api/roboflow_api.py
+++ src/beachbot_od/roboflow_api/roboflow_api.py
@@ -1,14 +1,18 @@
 import roboflow
 import keyring
 import os
+import yaml
 
 
-def get_dataset(ver=1: int, model_format="coco", location="./dataset", overwrite=False):
-    """
-    Downloads dataset from Roboflow
-    """
+def load_config(config_path):
+    # Load the YAML configuration file
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
 
-    # Access API from environment variable
+
+def connect() -> roboflow.Roboflow:
+    # Access API from huggingface secrets
     api_key = os.getenv("ROBOFLOW_KEY")
 
     if not api_key:
@@ -17,17 +21,42 @@ def get_dataset(ver=1: int, model_format="coco", location="./dataset", overwrite
 
     if not api_key:
         raise ValueError(
-            """API key not found in keyring.
+            """API key not found in keyring. 
         Make sure it is stored securely.
         HuggingFace Usage:
         os.getenv("ROBOFLOW_API_KEY")
-        Local Usage:
+        Local Usage: 
         pip install keyring
         python -c "import keyring; keyring.set_password('roboflow', 'api_key', 'your_actual_api_key')"
         """
         )
     rf = roboflow.Roboflow(api_key)
+    return rf
 
+
+def generate_version(config_path="../roboflow_version_config.yaml") -> int:
+    # Return version ID
+    # See https://docs.roboflow.com/api-reference/versions/create-a-project-version for more information
+
+    rf = connect()
+    project = rf.workspace().project("beach-cleaning-object-detection")
+
+    version: int
+    # Load default config settings
+    settings = load_config(config_path=config_path)
+
+    version = project.generate_version(settings)
+    print("Version ID:", version)
+    return version
+
+
+def get_dataset(
+    ver: int = 1, model_format="coco", location="./dataset", overwrite=False
+):
+    """
+    Downloads dataset from Roboflow
+    """
+    rf = connect()
     project = rf.workspace("okinawaaibeachrobot").project(
         "beach-cleaning-object-detection"
     )
@@ -43,7 +72,3 @@ def get_dataset(ver=1: int, model_format="coco", location="./dataset", overwrite
 
     dataset = version.download(model_format, location, overwrite)
     print("Dataset downloaded at " + dataset.location)
-
-
-if __name__ == "__main__":
-    get_dataset()
